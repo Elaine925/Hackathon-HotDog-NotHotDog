b6acf7962acf2b32fd66e70bc23a0e0e6fa7a1b1
Yilan Chen <yilan.c@Ys-Macbook-Pro.local>
2024-03-15 14:18:55 -0700

    added another_hotdog.py

--- another_hotdog.py
+++ another_hotdog.py
@@ -19,13 +19,8 @@ from keras.preprocessing.image import load_img, img_to_array
 from PIL import Image
 
 
-# model_path = "./MobileNet.h5"
-# model = tf.keras.models.load_model(model_path)
 
-# Provide path to your pickled model file
-# model_path = "../Hackupdated_mobilenet.sav"  
-# with open(model_path, 'rb') as f:
-#     model = pickle.load(f)
+# Function to load and preprocess the image
 def preprocess_image(image):
     # Resize the image to match the input size of your CNN model
     image = image.resize((256, 256))  # Assuming your model expects 224x224 input
@@ -37,49 +32,32 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)
     return image
 
-# def predict(image, model):
-#     # Resize image to match model input shape
-#     image = image.resize((224, 224))
-#     # Convert PIL image to numpy array
-#     image_array = tf.keras.preprocessing.image.img_to_array(image)
-#     # Normalize pixel values
-#     image_array /= 255.0
-#     # Expand dimensions to match model input shape
-#     image_array = tf.expand_dims(image_array, 0)
-#     # Predict class probabilities
-#     predictions = model.predict(image_array)
-#     return predictions
+
 
 st.title("Hot Dog or *NOT* Hot Dog :hotdog:")
 st.subheader(':blue[Delivered by *GIRLPOWER*] \n Nkechi Goodacre, Rajashree Choudhary, Elaine Chen :female-student:')
 
 
 
-def image_uploader():
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+uploaded_image = st.file_uploader("Upload an image", type=['png', 'jpeg', 'jpg'])
 
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-        model = keras.models.load_model('./MobileNet.h5')
-        preprocessed_image = preprocess_image(image)
-        pred = model.predict(preprocessed_image)[0][0]
-
-        return image
-    else:
-        st.write("Please upload a hotdog image file.")
-
-uploaded_image = image_uploader()
 if uploaded_image is not None:
-    # Display uploaded image
-    image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(uploaded_image)
 
+
+    image = Image.open(uploaded_image)
     preprocessed_image = preprocess_image(image)
 
 
     model = keras.models.load_model('./MobileNet.h5')
     pred = model.predict(preprocessed_image)[0][0]
+
+    
+    # if (pred < 0.5):
+    #     pred_image = 'img/itshotdog.png'
+    # else: 
+    #     pred_image = 'img/nothotdog.png'
+
 
     # Make predictions
     if st.button("Classify"):
