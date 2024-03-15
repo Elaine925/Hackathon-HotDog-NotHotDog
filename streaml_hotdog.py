85e39b81b766aec0c4238fe29d18404563d56d16
Yilan Chen <yilan.c@Ys-Macbook-Pro.local>
2024-03-15 13:03:35 -0700

    h

--- streaml_hotdog.py
+++ streaml_hotdog.py
@@ -1,43 +1,31 @@
 import streamlit as st
 from PIL import Image
-import tensorflow
+import tensorflow as tf
 import os
 import shutil
+import pickle
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-<<<<<<< HEAD
+
+
 # Provide path to your pickled model file
-model_path = "finalized_model.sav"  
+model_path = "../Hackupdated_mobilenet.sav"  
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
-=======
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.callbacks import EarlyStopping
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from tensorflow.keras.utils import to_categorical
-
-def load_model(model_path):
-    model = tensorflow.keras.models.load_model(model_path)
-    return model
->>>>>>> main
-
+    
 def predict(image, model):
     # Resize image to match model input shape
     image = image.resize((224, 224))
     # Convert PIL image to numpy array
-    image_array = tensorflow.keras.preprocessing.image.img_to_array(image)
+    image_array = tf.keras.preprocessing.image.img_to_array(image)
     # Normalize pixel values
     image_array /= 255.0
     # Expand dimensions to match model input shape
-    image_array = tensorflow.expand_dims(image_array, 0)
+    image_array = tf.expand_dims(image_array, 0)
     # Predict class probabilities
     predictions = model.predict(image_array)
     return predictions
